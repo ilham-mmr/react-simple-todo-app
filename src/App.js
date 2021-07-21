e09adf273eb21c0165c6771edeb3ce9e2a625d66
Ilham Rachmat <54584081+ilham-mmr@users.noreply.github.com>
2021-07-21 10:14:17 +0700

    make todos be saved persistently in browser local storage.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,46 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import AddTodo from './Todos/AddTodo';
 import TodoItemsList from './Todos/TodoItemsList';
 function App() {
-    const [todoItems, setTodoItems] = useState([]);
 
-    const addTodoItemHandler = (todoItem) => {
-        setTodoItems(prevTodoItems => {
-            return [...prevTodoItems, { id: Math.random(), todoItem }];
-        })
+  const initializeTodoItems = () => {
+    const todoItems = localStorage.getItem('todoItems')
+    const parsedTodoItems = JSON.parse(todoItems);
+    if (parsedTodoItems) {
+      return parsedTodoItems;
     }
 
-    const deleteTodoItemHandler = id => {
-        setTodoItems(prevTodoItems => {
-            return prevTodoItems.filter((todoItem) => todoItem.id !== id);
-        })
-    }
+    return [];
+  }
+  const [todoItems, setTodoItems] = useState(() => initializeTodoItems());
 
-    const editTodoItemHandler = (id, updatedTodoItem) => {
-        setTodoItems(prevTodoItems => {
-            const idx = prevTodoItems.findIndex((todoItem) => todoItem.id === id);
-            prevTodoItems[idx] = { id: Math.random(), todoItem: updatedTodoItem };
-            console.log(prevTodoItems);
-            return prevTodoItems;
-        })
-    }
-    return <>
-        <AddTodo onAddTodoItem={addTodoItemHandler} />
-        <TodoItemsList todoItems={todoItems} onDeleteTodoItem={deleteTodoItemHandler} onEditTodoItem={editTodoItemHandler} />
-    </>
+  useEffect(() => {
+    localStorage.setItem('todoItems', JSON.stringify(todoItems));
+  }, [todoItems])
+
+  const addTodoItemHandler = (todoItem) => {
+    setTodoItems(prevTodoItems => {
+      return [...prevTodoItems, { id: Math.random(), todoItem }];
+    })
+  }
+
+  const deleteTodoItemHandler = id => {
+    setTodoItems(prevTodoItems => {
+      return prevTodoItems.filter((todoItem) => todoItem.id !== id);
+    })
+  }
+
+  const editTodoItemHandler = (id, updatedTodoItem) => {
+    setTodoItems(prevTodoItems => {
+      const idx = prevTodoItems.findIndex((todoItem) => todoItem.id === id);
+      prevTodoItems[idx] = { id: Math.random(), todoItem: updatedTodoItem };
+      return [...prevTodoItems,];
+    })
+  }
+  return <>
+    <AddTodo onAddTodoItem={addTodoItemHandler} />
+    <TodoItemsList todoItems={todoItems} onDeleteTodoItem={deleteTodoItemHandler} onEditTodoItem={editTodoItemHandler} />
+  </>
 
 
 
