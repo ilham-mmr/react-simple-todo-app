bcbaeed2a21e197d81f922dfad0216c24f7d695a
Ilham Rachmat <54584081+ilham-mmr@users.noreply.github.com>
2021-07-22 19:40:22 +0700

    refactor code to implement useContext

--- src/Todos/TodoItemsList.js
+++ src/Todos/TodoItemsList.js
@@ -1,19 +1,21 @@
+import { useContext } from "react";
+import { TodoContext } from "../Contexts/todos-context";
 import Card from "../UI/Card";
 import TodoItem from "./TodoItem";
 import styles from "./TodoItemsList.module.css";
 
-const TodoItemsList = props => {
-    return <Card className={styles['todo-items']}>
-        {props.todoItems.length > 0 ?
-            <ul>
-                {props.todoItems.map(todo => <TodoItem key={todo.id} id={todo.id} todoItem={todo.todoItem}
-                    onDeleteTodoItem={props.onDeleteTodoItem} onEditTodoItem={props.onEditTodoItem}
-                />)}
-            </ul>
-            :
-            <p >No Todo. Add Now!</p>
-        }
-    </Card >
+const TodoItemsList = () => {
+  const todosCtx = useContext(TodoContext);
+
+  return <Card className={styles['todo-items']}>
+    {todosCtx.todoItems.length > 0 ?
+      <ul>
+        {todosCtx.todoItems.map(todo => <TodoItem key={todo.id} id={todo.id} todoItem={todo.todoItem} />)}
+      </ul>
+      :
+      <p >No Todo. Add Now!</p>
+    }
+  </Card >
 }
 
 export default TodoItemsList;
